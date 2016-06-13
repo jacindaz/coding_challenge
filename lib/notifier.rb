a692a9e621e7adb52924183bdd5e8983b744fced
Jacinda <jacindaz@gmail.com>
2016-06-13 08:42:03 -0400

    Add error handling for Twilio API
     - add pry for debugging

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,11 +1,12 @@
 require 'twilio-ruby'
+require 'pry'
 
 module Notifier
   def self.send_sms_notifications(image_search_terms, text_number, text_body = "Text from Twilio")
     twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-    client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
+    client = Twilio::REST::Client.new(twilio_account_sid, twilio_auth_token)
     send_sms(client, text_number, text_body, image_search_terms)
   end
 
@@ -14,11 +15,17 @@ module Notifier
   def self.send_sms(client, to_number, text_body, image_search_terms)
     twilio_number = ENV['TWILIO_FROM_NUMBER']
 
-    client.account.messages.create(
-      from: twilio_number,
-      to: to_number,
-      body: text_body,
-      media_url: ::Giphy.new.get_image_data(image_search_terms)
-    )
+    begin
+      client.account.messages.create(
+        from: twilio_number,
+        to: to_number,
+        body: text_body,
+        media_url: ::Giphy.new(image_search_terms).giphy_image_search_url
+      )
+    rescue Twilio::REST::RequestError => e
+      e.message
+    rescue Exception => e
+      e.message
+    end
   end
 end
