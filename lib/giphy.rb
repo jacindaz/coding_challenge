73ca265fba71031a6ea4e64ee57bdd19b041cae3
Jacinda <jacindaz@gmail.com>
2016-06-13 08:40:48 -0400

    Class to search Giphy public API
     - hardcoded api key, since it's public

--- lib/giphy.rb
+++ lib/giphy.rb
@@ -3,20 +3,29 @@ require 'json'
 require 'open-uri'
 
 class Giphy
-  def initialize
+  def initialize(search_params)
+    @search_params = search_params
     @api_endpoint = 'http://api.giphy.com/v1/gifs/search?q='
   end
 
-  def get_image_data(search_params)
-    uri = URI(construct_api_endpoint(search_params))
+  def giphy_image_search_url
+    return_image_url(get_image_data)
+  end
+
+  def get_image_data
+    uri = URI(construct_api_endpoint)
     response = Net::HTTP.get(uri)
-    JSON.parse(response)['data'].first['url']
+    JSON.parse(response)['data']
   end
 
   private
 
-  def construct_api_endpoint(search_params)
+  def return_image_url(data)
+    data.first['images']['downsized_medium']['url']
+  end
+
+  def construct_api_endpoint
     # http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
-    @api_endpoint + 'search_params' + "&api_key=dc6zaTOxFJmzC"
+    @api_endpoint + "#{@search_params}&api_key=dc6zaTOxFJmzC"
   end
 end
